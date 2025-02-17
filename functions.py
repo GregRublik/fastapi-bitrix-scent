94768ae2607f2afdbb5595ef48e4f86a3ed8bf69
Grigory Ivanov <grigorashekivanov2@gmail.com>
2025-02-17 09:18:21 +0300

    та же ошибка, но в другом месте

--- functions.py
+++ functions.py
@@ -2,4 +2,4 @@ from core.config import settings
 
 
 def check_token(client_secret):
-    assert client_secret == settings.secret, "Invalid Token"
+    assert client_secret == settings.client_secret, "Invalid Token"
