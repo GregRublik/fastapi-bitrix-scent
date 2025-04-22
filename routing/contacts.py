09cac456b3403e73f79693f0990ce5c80c645318
Grigory Ivanov <grigorashekivanov2@gmail.com>
2025-04-22 15:56:03 +0300

    исправил ошибку при которой дата не фиксировалась в компаниях и контактах

--- routing/contacts.py
+++ routing/contacts.py
@@ -30,71 +30,73 @@ async def activity_update(
         })
     activity = await activity.json()
 
-    case_id = activity['result']['ID']
     provider_type_id = activity['result']['PROVIDER_TYPE_ID']
     provider_id = activity['result']['PROVIDER_ID']
-    owner_id = activity['result']['OWNER_ID']
-    owner_type_id = activity['result']['OWNER_TYPE_ID']
-
-    async def update_time_activity():
-        # owner_type_id - 3 contact, 4 company
-        if owner_type_id == '3':
-
-            # список компаний которые привязаны к данному контакту
-            list_company = await session.post(
-                url=f"{settings.portal_url}rest/crm.contact.company.items.get.json",
-                json={
-                    'auth': access[0],
-                    'id': owner_id,
-                }
-            )
-            list_company= await list_company.json()
-            # обновление "даты последнего контакта" во всех компаниях связанных с контактом
-            for company in list_company['result']:
-                await session.post(
-                    url=f"{settings.portal_url}rest/crm.company.update.json",
-                    json={
-                        'auth': access[0],
-                        'id': company['COMPANY_ID'],
-                        'fields': {
-                            'UF_CRM_1744886793': datetime.datetime.now().isoformat()
-                        }
-                    }
-                )
-
-            return await session.post(
-                url=f"{settings.portal_url}rest/crm.contact.update.json",
-                json={
-                    'auth': access[0],
-                    'id': owner_id,
-                    'fields': {
-                        'UF_CRM_1744886984': datetime.datetime.now().isoformat()
-                    }
-                }
-            )
-        elif owner_type_id == '4':
-            return await session.post(
-                url=f"{settings.portal_url}rest/crm.company.update.json",
-                json={
-                    'auth': access[0],
-                    'id': owner_id,
-                    'fields': {
-                        'UF_CRM_1744886793': datetime.datetime.now().isoformat()
-                    }
-                }
-            )
-
-    if owner_type_id not in ['3', '4']:
-        return {'status_code': 200, 'detail': 'owner not 3, 4'}
 
     # если это ненужное нам дело, то мы ничего не далаем
     if provider_type_id not in ['CALL', 'EMAIL'] and provider_id != 'IMOPENLINES_SESSION':
         return {'status_code': 200, 'detail': 'case is not call, email, message'}
 
-    # звонок
-    elif provider_type_id == 'CALL':
-        # print(f"Phone activity: {case_id}")
+    async def update_time_activity():
+        # owner_type_id - 2 deal, 3 contact, 4 company
 
+        list_owner = await session.post(
+            url=f"{settings.portal_url}rest/crm.activity.binding.list.json",
+            json={
+                'auth': access[0],
+                'activityId': activity_id,
+            }
+        )
+        list_owner = await list_owner.json()
+        for owner in list_owner['result']:
+            if owner['entityTypeId'] == 3:
+                list_company = await session.post(
+                    url=f"{settings.portal_url}rest/crm.contact.company.items.get.json",
+                    json={
+                        'auth': access[0],
+                        'id': owner['entityId'],
+                    }
+                )
+                list_company = await list_company.json()
+                # обновление "даты последнего контакта" во всех компаниях связанных с контактом
+                for company in list_company['result']:
+                    await session.post(
+                        url=f"{settings.portal_url}rest/crm.company.update.json",
+                        json={
+                            'auth': access[0],
+                            'id': company['COMPANY_ID'],
+                            'fields': {
+                                'UF_CRM_1744886793': datetime.datetime.now().isoformat()
+                            }
+                        }
+                    )
+
+                updated_contact = await session.post(
+                    url=f"{settings.portal_url}rest/crm.contact.update.json",
+                    json={
+                        'auth': access[0],
+                        'id': owner['entityId'],
+                        'fields': {
+                            'UF_CRM_1744886984': datetime.datetime.now().isoformat()
+                        }
+                    }
+                )
+                return await updated_contact.json()
+            elif owner['entityTypeId'] == 4:
+                updated_contact = await session.post(
+                    url=f"{settings.portal_url}rest/crm.company.update.json",
+                    json={
+                        'auth': access[0],
+                        'id': owner['entityId'],
+                        'fields': {
+                            'UF_CRM_1744886793': datetime.datetime.now().isoformat()
+                        }
+                    }
+                )
+                updated_contact = await updated_contact.json()
+                return updated_contact
+    # звонок
+    if provider_type_id == 'CALL':
         # если это пропущенный звонок, то мы ничего не делаем
         if activity['result']['SETTINGS'].get('MISSED_CALL', False):
             pass
@@ -107,15 +109,13 @@ async def activity_update(
 
     # почта
     elif provider_type_id == 'EMAIL':
-        # print(f"Email activity: {case_id}")
-
         # только если это новое письмо (вход, исход)
         if data.get('event') == 'ONCRMACTIVITYADD':
             updated_owner = await update_time_activity()
 
     # мессенджер
     elif provider_id == 'IMOPENLINES_SESSION':
-        # print(f"Message activity: {case_id}")
+
         if activity['result']['COMPLETED'] == 'Y':
             pass
         else:
