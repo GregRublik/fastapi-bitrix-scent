4718e5c811c322816a2c8aa1275821b0d6ece5b4
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-02-21 14:53:52 +0300

    change venv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,8 +3,8 @@ sys.path.append('/src')
 
 import os
 
-# INTERP = os.path.expanduser("/var/www/u2842936/data/venv/bin/python")
-INTERP = os.path.expanduser("/var/www/u2842936/data/www/sporbita-developers.ru/.venv/bin/python")
+INTERP = os.path.expanduser("/var/www/u2842936/data/venv/bin/python")
+# INTERP = os.path.expanduser("/var/www/u2842936/data/www/sporbita-developers.ru/.venv/bin/python")
 
 if sys.executable != INTERP:
    os.execl(INTERP, INTERP, *sys.argv)
