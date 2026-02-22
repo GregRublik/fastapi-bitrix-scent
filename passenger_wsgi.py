86f2dffd4525475ab858da9fca802b5b7a1c1107
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-02-22 11:00:34 +0300

    fix pass file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,11 @@
 import os
 import sys
-sys.path.append('/src')
+
+# путь к корню проекта
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# добавляем корень проекта в sys.path
+sys.path.insert(0, PROJECT_ROOT)
 
 INTERP = os.path.expanduser("/var/www/u2842936/data/venv/bin/python")
 # INTERP = os.path.expanduser("/var/www/u2842936/data/www/sporbita-developers.ru/.venv/bin/python")
